2d7246a8d9fe4c8b3ee17035172b0b2c457bda04
Humberto Fernandes <humbertofernandes08@hotmail.com>
2022-09-16 23:27:13 -0300

    adicionando controller da lista

---
+++ src/main/java/com/lista/rest/api/controllers/ListaController.java
@@ -0,0 +1,44 @@
+package com.lista.rest.api.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lista.rest.api.configs.ControllerConfig;
+
+@RestController
+@RequestMapping(ControllerConfig.URL_API + "lista")
+@CrossOrigin(origins = "*")
+public class ListaController {
+	
+
+	@PostMapping
+	public void cadastraLista() {
+		
+	}
+	
+	@PutMapping
+	public void alteraLista() {
+		
+	}
+	
+	@GetMapping
+	public void listaTodasListas() {
+		
+	}
+	
+	@GetMapping
+	public void listaPeloIdDaLista() {
+		
+	}
+	
+	@DeleteMapping
+	public void deletaLista() {
+		
+	}
+
+}
