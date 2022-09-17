5939a4655804be6966aaf9e3e0f006c63bead6d7
Humberto Fernandes <humbertofernandes08@hotmail.com>
2022-09-16 23:04:35 -0300

    arquivos iniciais do projeto

---
+++ src/test/java/com/lista/rest/api/ListaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lista.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
